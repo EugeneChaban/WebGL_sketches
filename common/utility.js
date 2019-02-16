43b6389f907302d88e9ba8b61e88bbaf58700387
EugeneChaban <chaban.genya@gmail.com>
2019-02-16 18:43:41 +0300

    Update utility.js
    delete functionstart() and global values

--- common/utility.js
+++ common/utility.js
@@ -3,21 +3,6 @@ function splitLines(text) {
     return text.split(/\r?\n|\r/);
 }
 
-let g_pntText = null;
-let g_polText = null;
-
-function start() {
-    const pntLines = splitLines(g_pntText);
-    const polLines = splitLines(g_polText);
-    const tblk = document.getElementById("text_block");
-    if (tblk) {
-        const pntInfo = `pnt: ${pntLines.length} lines`;
-        const polInfo = `pol: ${polLines.length} lines`;
-        tblk.innerHTML = `${pntInfo}<br>${polInfo}`;
-    }
-}
-
-
 // convert degrees to radians
 function radians(degs) {
     return degs * (Math.PI / 180.0);
