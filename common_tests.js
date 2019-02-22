6fb54756601bbd229feca58197e936df11a22ab2
IronSkill <chaban.genya@gmail.com>
2019-02-22 13:43:16 +0200

    Delete mistakes

--- common_tests.js
+++ common_tests.js
@@ -12,7 +12,7 @@ function testCompareFloat() {
     if (compareFloat(x, x + e*1.1, e)) {
         return false;
     }
-    if (!compareFloat(Math.sqrt(2), 99\70) {
+    if (!compareFloat(Math.sqrt(2), 99/70)) {
 		return false;
     }
     return true;
