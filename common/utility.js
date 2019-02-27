924544456c02f6c112927ab317aaf6dd4b5d2ff5
IronSkill <chaban.genya@gmail.com>
2019-02-27 23:13:27 +0200

    Updated

--- common/utility.js
+++ common/utility.js
@@ -3,22 +3,22 @@
 
 // split text into lines, handling different newline formats
 function splitLines(text) {
-    return text.split(/\r?\n|\r/); 
+	return text.split(/\r?\n|\r/); 
 }
 
 // convert degrees to radians
 function radians(degs) {
-    return degs * (Math.PI / 180.0);
+	return degs * (Math.PI / 180.0);
 }
 
 // converts radians to degrees
 function degrees(rads) {
-    return rads * (180.0 / Math.PI);
+	return rads * (180.0 / Math.PI);
 }
 
 // comparing floating-point numbers for near-equality
 function compareFloat(a, b, e = 0.001) {    // e defines acceptable error
-    return Math.abs(a - b) < e;
+	return Math.abs(a - b) < e;
 }
 
 
